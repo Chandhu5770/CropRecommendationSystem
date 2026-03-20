9d33f47ba701be6eff6a5263cae54a98747469d2
Chandhu5770 <barkamchandhu7@gmail.com>
2026-03-20 23:23:37 +0100

    fixed file names

---
+++ crop_recommendation.py
@@ -0,0 +1,15 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+
+======================================================================== RESTART: C:/Users/ADMIN/projects/py.py ========================================================================
+Crop Recommendation System
+
+Enter soil type (loamy, sandy, clay, silt, peat, chalk): loamy
+Enter rainfall (mm): 600
+Enter temperature (°C): 28
+
+Recommended crops:
+- Rice
+- Sugarcane
+- Maize
